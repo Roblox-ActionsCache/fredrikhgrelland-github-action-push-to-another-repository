a63a61b8ddba3158085078bd5ab8c78ca3201e08
Fredrik Hoem Grelland <fredrikhoem.grelland@skatteetaten.no>
2020-06-29 15:14:43 +0200

    Add github_workspace to it

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Starts"
 FOLDER="$1"
 GITHUB_USERNAME="$2"
 GITHUB_REPO="$3"
-USER_EMAIL="$4"
+USER_EMAIL="$5"
 
 CLONE_DIR=$(mktemp -d)
 
@@ -13,16 +13,18 @@ git config --global user.email "$USER_EMAIL"
 git config --global user.name "$GITHUB_USERNAME"
 git clone "https://$API_TOKEN_GITHUB@github.com/$GITHUB_USERNAME/$GITHUB_REPO.git" "$CLONE_DIR"
 
+echo "Before sync:"
 ls -la "$CLONE_DIR"
 
-# Copy files into the git and deletes all git
-find "$CLONE_DIR" | grep -v "^$CLONE_DIR/\.git" | grep -v "^$CLONE_DIR$" | xargs rm -rf # delete all files (to handle deletions)
+echo "Folder to be synced:"
+ls -la "$GITHUB_WORKSPACE/$FOLDER"
 
-ls -la "$CLONE_DIR"
-
-cp -r "$FOLDER"/* "$CLONE_DIR"
+cp -r "$GITHUB_WORKSPACE/$FOLDER/*" "$CLONE_DIR"
 
 cd "$CLONE_DIR"
+
+echo "After sync:"
+ls -la
 
 git add .
 git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA)"
